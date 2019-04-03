329fcb8a8da0247d05acdfb42933ac7af3dbcf7d
Gavin O'Connor <gedmund@gmail.com>
2019-04-03 18:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // function accepts an array of names and returns a numbered string.
 function dwarfRollCall(dwarves) {
   let dwarvesArray = [];
-  for (var i = 0; i < dwarves.length; i++) {
-    dwarvesArray.push(`${[i + 1]}. ${dwarves[i]} `)
-  }
+  // for (var i = dwarves.length - 1; i >= 0; i--) {
+  //
+  //     dwarvesArray.push(`${[i + 1]}. ${dwarves[i]} `)
+  //
+  // }
   return dwarvesArray.join('')
 }
 
